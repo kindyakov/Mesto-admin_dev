6c330f99439f8e1ecbc56e27b3af00383d68f818
kindyakov <kindyakov_2016@mail.ru>
2024-08-28 14:52:17 +0300

    20240828145215

--- src/js/settings/api.js
+++ src/js/settings/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://store-franchise.ru' // store-demo-test.ru
+const baseURL = 'https://store-demo-test.ru' // store-franchise.ru
 
 axios.defaults.timeout = 30000
 
@@ -8,6 +8,38 @@ export const api = axios.create({ baseURL })
 
 api.interceptors.response.use(
   response => {
+    const [path] = response.config.url.split('/').filter(el => el !== '')
+
+    switch (path) {
+      case '_dashboard_warehouse_':
+        const [d] = response.data.rented_cnt.filter(obj => +obj.rented == -2)
+
+        if (d) {
+          response.data.rented_cnt = response.data.rented_cnt.map(obj => {
+            if (+obj.rented == 0) {
+              obj.area = obj.area + d.area
+              obj.cnt = obj.cnt + d.area
+              obj.rate = obj.rate + d.rate
+            }
+
+            return obj
+          })
+        }
+
+        break;
+      case '_main_rooms_':
+        response.data.plan_rooms = response.data.plan_rooms.map(obj => {
+          if (+obj.rented == -2) {
+            obj.rented = 0
+          }
+          return obj
+        })
+
+        break;
+      default:
+        break;
+    }
+
     return response;
   },
   error => {
